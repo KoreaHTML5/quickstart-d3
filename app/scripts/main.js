5908900979443812e25549d791ca2475e2586279
jeonghwan <ej88ej@gmail.com>
2015-09-05 16:54:34 +0900

    Bar chart

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,49 @@
-/**
- * Created by Chris on 2015. 9. 4..
- */
+'use strict';
+
+(function () {
+  var w = 300,
+      h = 200,
+      data = [10, 50, 100, 150, 200, 150, 100, 50, 10],
+      svg = d3
+          .select('body')
+          .append('svg')
+          .attr('width', w)
+          .attr('height', h);
+
+  svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', function (d, i) {
+        return i * (w / data.length);
+      })
+      .attr('y', function (d) {
+        return h - d;
+      })
+      .attr('width', w / data.length - 1)
+      .attr('height', function (d) {
+        return d;
+      })
+      .attr('fill', function (d) {
+        return 'hotpink';
+      });
+
+  svg.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .text(function(d) {
+        return d;
+      })
+      .attr("x", function(d, i) {
+        return i * (w / data.length) + (w / data.length) / 2;
+      })
+      .attr("y", function(d) {
+        return h - d + 10;
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("fill", "black")
+      .attr("text-anchor", "middle");
+
+})();
